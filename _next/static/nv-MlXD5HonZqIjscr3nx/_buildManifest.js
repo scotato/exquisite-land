7d13ede1e047618e7bf5199918ce83a9ceb4f203
cjpais <cjpais@users.noreply.github.com>
2021-09-11 21:33:52 +0000

    deploy: cb25492a5e6231ce904b8f92b50a32a12b22fd5d

---
+++ _next/static/nv-MlXD5HonZqIjscr3nx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/882-91f8f114fe523eb94f29.js","static/chunks/pages/index-70b1fe7935dc4b4ee4fc.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
