f58c799c257861887a80eff7d51281d41ce9ae0d
cjpais <cjpais@users.noreply.github.com>
2021-09-11 22:37:07 +0000

    deploy: 520fab2cfc5daa0d97d6329f99f3fa76338bab68

--- _next/static/LtgIAtddf7mYINbj3wfzN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/882-91f8f114fe523eb94f29.js","static/chunks/pages/index-6bc122a88b8fd0e213d4.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
