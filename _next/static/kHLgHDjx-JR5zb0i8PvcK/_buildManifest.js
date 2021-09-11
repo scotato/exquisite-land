11b40be41b70247cc87c1391c68ae0c3773e89d5
shahruz <shahruz@users.noreply.github.com>
2021-09-11 20:20:27 +0000

    deploy: 31a3d0d6b97920f4be92241a2658ee69da9aadc9

---
+++ _next/static/kHLgHDjx-JR5zb0i8PvcK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/882-91f8f114fe523eb94f29.js","static/chunks/pages/index-f45489fd83ed0d78ca04.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
